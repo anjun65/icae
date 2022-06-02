52a734e22cc32672785e51b327f72ebc0053c13a
anjun65 <hajrulkhaira@gmail.com>
2022-06-02 16:27:18 +0700

    penambahan logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'ICAE 2022') }}</title>
+        <title>ICAE Polibatam</title>
+
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
