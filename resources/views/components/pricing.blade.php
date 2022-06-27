28f4a6d97f57464a8dff80a2c4ea41541e0e81ad
anjun65 <hajrulkhaira@gmail.com>
2022-06-27 16:17:06 +0700

    wip

--- resources/views/components/pricing.blade.php
+++ resources/views/components/pricing.blade.php
@@ -19,9 +19,9 @@
             <div class="card-actions justify-center">
             
               
-            <x-jet-button type="button" class="mt-2">
+            <x-button-dark href="{{ route('register') }}" class="mt-2">
                 {{ __('Register') }}
-            </x-jet-button>
+            </x-button-dark>
             </div>
           </div>
         </div>
@@ -43,9 +43,9 @@
             <div class="card-actions justify-center">
             
               
-            <x-jet-button type="button" class="mt-2">
+            <x-button-dark href="{{ route('register') }}" class="mt-2">
                 {{ __('Register') }}
-            </x-jet-button>
+            </x-button-dark>
             </div>
           </div>
         </div>
@@ -65,9 +65,9 @@
               <li>Conference Attendees (Indonesia non Author) Free</li>
             </p>
             <div class="card-actions justify-center">
-            <x-jet-button type="button" class="mt-2">
+            <x-button-dark href="{{ route('register') }}" class="mt-2">
                 {{ __('Register') }}
-            </x-jet-button>
+            </x-button-dark>
             </div>
           </div>
         </div>
@@ -77,63 +77,3 @@
 </div>
 
 </div>
-
-{{-- <div>
-  <div class="bg-white">
-  <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-    <h2 class="text-2xl font-extrabold tracking-tight text-gray-900 text-center mb-4">Pricing</h2>
-
-    <div class="grid grid-cols-3 gap-3">
-        <div class="w-80 rounded-2xl border shadow py-12 px-8 hover:-translate-y-1 hover:shadow-2xl delay-75 duration-100">
-
-            <!-- Header & Price -->
-            <p class="text-3xl text-gray-700 font-semibold"> Premium </p>
-            <p class="text-xl text-gray-700 font-semibold mt-1"> Rp 220.000 </p>
-            <p class="text-sm text-gray-700 font-semibold mt-1"> IDR per month </p>
-
-            <!-- Description -->
-            <p class="text-sm text-gray-700 font-light mt-5 leading-7"> The VIP support plan for businesses ready to monetize and sell on a larger scale. </p>
-
-            <!-- CTA Button -->
-            <x-jet-button type="button" class="mt-2">
-                {{ __('Register') }}
-            </x-jet-button>
-
-        </div>
-        <div class="w-80 rounded-2xl border shadow py-12 px-8 hover:-translate-y-1 hover:shadow-2xl delay-75 duration-100">
-
-            <!-- Header & Price -->
-            <p class="text-3xl text-gray-700 font-semibold"> Premium </p>
-            <p class="text-xl text-gray-700 font-semibold mt-1"> Rp 220.000 </p>
-            <p class="text-sm text-gray-700 font-semibold mt-1"> IDR per month </p>
-
-            <!-- Description -->
-            <p class="text-sm text-gray-700 font-light mt-5 leading-7"> The VIP support plan for businesses ready to monetize and sell on a larger scale. </p>
-
-            <!-- CTA Button -->
-            <x-jet-button type="button" class="mt-2">
-                {{ __('Register') }}
-            </x-jet-button>
-
-        </div>
-        <div class="w-80 rounded-2xl border shadow py-12 px-8 hover:-translate-y-1 hover:shadow-2xl delay-75 duration-100">
-
-            <!-- Header & Price -->
-            <p class="text-3xl text-gray-700 font-semibold"> Premium </p>
-            <p class="text-xl text-gray-700 font-semibold mt-1"> Rp 220.000 </p>
-            <p class="text-sm text-gray-700 font-semibold mt-1"> IDR per month </p>
-
-            <!-- Description -->
-            <p class="text-sm text-gray-700 font-light mt-5 leading-7"> The VIP support plan for businesses ready to monetize and sell on a larger scale. </p>
-
-            <!-- CTA Button -->
-            <x-jet-button type="button" class="mt-2">
-                {{ __('Register') }}
-            </x-jet-button>
-
-        </div>
-    </div>
-  </div>
-  </div>
-
-</div> --}}
