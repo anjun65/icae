e92f139b5eb43f74fd0d06c18525e6a0bd244775
anjun65 <hajrulkhaira@gmail.com>
2023-07-14 16:00:26 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,6 @@
         <x-hero2023/>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                 
                  <x-about2023/>
             </div>
         </div>
