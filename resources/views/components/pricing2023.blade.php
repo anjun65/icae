0cfdea105297fc7a4e44ede389fcd8fcfe534cad
anjun65 <hajrulkhaira@gmail.com>
2023-08-23 14:38:00 +0700

    test

--- resources/views/components/pricing2023.blade.php
+++ resources/views/components/pricing2023.blade.php
@@ -3,7 +3,7 @@
       <div class="mx-auto max-w-screen-md sm:text-center">
           <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl ">Register Now </h2>
           
-          <p class="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl "> Register Now for Rp. 2.750.000<br/>Contact : icaess@polibatam.ac.id</p>
+          <p class="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl "> Register Now for Rp. 2.750.000<br/>Contact : icae@polibatam.ac.id</p>
           
       </div>
   </div>
