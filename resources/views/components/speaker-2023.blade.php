cc12a0b885494adcc149db56871e40d388a60556
anjun65 <hajrulkhaira@gmail.com>
2023-08-29 15:16:05 +0700

    test

--- resources/views/components/speaker-2023.blade.php
+++ resources/views/components/speaker-2023.blade.php
@@ -27,24 +27,3 @@
   </div>
 </section>
 
-{{-- <div>
-    <div class="grid grid-cols-1 md:grid-cols-2">
-        <div class="flex items-center justify-center h-screen bg-gradient-to-br from-indigo-500 to-indigo-800">
-          <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
-            <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="product designer">
-            <h1 class="text-lg text-gray-700">Dr.Ir. Basuki Rahmatul Alam</h1>
-            <h3 class="text-sm text-gray-400 ">Retired ITB lecturer/ founder TFME/ Polibatam lecturer / Senior Member IEEE</h3>
-            
-          </div>
-        </div>
-
-        <div class="flex items-center justify-center h-screen bg-gradient-to-br from-indigo-500 to-indigo-800">
-          <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
-            <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="product designer">
-            <h1 class="text-lg text-gray-700">Dr MK Radhakrishnan</h1>
-            <h3 class="text-sm text-gray-400 ">NanoRel - LLP Technical Consultant, Spore / VP IEEE EDS</h3>
-            </div>
-        </div>
-    </div>
-    
-</div> --}}
