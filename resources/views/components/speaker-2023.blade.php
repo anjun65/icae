56151ed2ca01c82a1142bf74b054866d1bdfbf13
anjun65 <hajrulkhaira@gmail.com>
2023-08-29 15:06:02 +0700

    test

---
+++ resources/views/components/speaker-2023.blade.php
@@ -0,0 +1,50 @@
+<section class="bg-white ">
+  <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div class="mx-auto max-w-screen-md sm:text-center">
+          <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl ">Speaker </h2>
+
+          <div class="grid max-w-screen-xl grid-cols-1 gap-8 p-4 mx-auto text-gray-900 md:grid-cols-2 sm:p-8">
+                
+                <div class="flex flex-col items-center justify-center ">
+                  <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
+                    <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="{{ asset('img/speaker/Dr.Ir. Basuki Rahmatul Alam.jpg') }}" alt="Speaker">
+                    <h1 class="text-lg text-gray-700">Dr.Ir. Basuki Rahmatul Alam</h1>
+                    <h3 class="text-sm text-gray-400 ">Retired ITB lecturer/ founder TFME/ Polibatam lecturer / Senior Member IEEE</h3>
+
+                  </div>
+                </div>
+                <div class="flex flex-col items-center justify-center ">
+                  <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
+                    <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="{{ asset('img/speaker/Dr MK Radhakrishnan.jpg') }}" alt="Speaker">
+                    <h1 class="text-lg text-gray-700">Dr MK Radhakrishnan</h1>
+                    <h3 class="text-sm text-gray-400 ">NanoRel - LLP Technical Consultant, Spore / VP IEEE EDS<br/><span class="text-red-600">* currently in the confirmation stage</span></h3>
+
+                  </div>
+                </div>
+          </div>
+
+      </div>
+  </div>
+</section>
+
+{{-- <div>
+    <div class="grid grid-cols-1 md:grid-cols-2">
+        <div class="flex items-center justify-center h-screen bg-gradient-to-br from-indigo-500 to-indigo-800">
+          <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
+            <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="product designer">
+            <h1 class="text-lg text-gray-700">Dr.Ir. Basuki Rahmatul Alam</h1>
+            <h3 class="text-sm text-gray-400 ">Retired ITB lecturer/ founder TFME/ Polibatam lecturer / Senior Member IEEE</h3>
+            
+          </div>
+        </div>
+
+        <div class="flex items-center justify-center h-screen bg-gradient-to-br from-indigo-500 to-indigo-800">
+          <div class="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
+            <img class="mb-3 w-32 h-32 rounded-full shadow-lg mx-auto" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="product designer">
+            <h1 class="text-lg text-gray-700">Dr MK Radhakrishnan</h1>
+            <h3 class="text-sm text-gray-400 ">NanoRel - LLP Technical Consultant, Spore / VP IEEE EDS</h3>
+            </div>
+        </div>
+    </div>
+    
+</div> --}}
